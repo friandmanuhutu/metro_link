b60ad3d0f31343bddd8c2d66f4219980b38e776b
friandmanuhutu <friandjacnusmanuhutu@gmail.com>
2024-06-03 23:49:07 +0700

    page give us rating + css tipsi-tipsi

--- resources/views/penilaian.blade.php
+++ resources/views/penilaian.blade.php
@@ -4,48 +4,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
-    <link rel="stylesheet" href="/css/createAgenda.css">
+    <title>RATING</title>
+    <link rel="stylesheet" href="/css/penilaian.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
-    <div class="container pt-4 bg-white">
-        <div class="row">
-          <div class="col-md-8 col-xl-6">
-            <h1>Ratting</h1>
-            <hr>
-
-            <form action="/metrolink/agenda_kota/store" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <label class="form-label" for="nama_peneyelenggara">Nama Penyelenggara</label>
-                    <input type="text" id="nama_peneyelenggara" name="nama_penyelenggara" placeholder="Masukkan Nama Penyelenggara Event" value="{{ old('nama_penyelenggara') }}"
-                        class="form-control @error('nama_penyelenggara') is-invalid @enderror">
-                    @error('nama_penyelenggara')
-                    <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="mb-3">
-                    <label class="form-label" for="deskripsi">Deskripsi Agenda</label>
-                    <textarea class="form-control" id="deskripsi" rows="3" placeholder="Masukan Deskripsi Event" name="deskripsi">{{ old('deskripsi') }}</textarea>
-                </div>
-
-                <div class="mb-3">
-                    <label class="form-label" for="tanggal_pelaksanaan">Tanggal Pelaksanaan</label>
-                    <input type="date" id="tanggal_pelaksanaan" name="tanggal_pelaksanaan"
-                        class="form-control @error('tanggal_pelaksanaan') is-invalid @enderror">
-                    @error('tanggal_pelaksanaan')
-                    <div class="text-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="submit" id="submit-agenda" name="submit-agenda">
-                </div>
-
-            </form>
-
-          </div>
+    <div class="container">
+        <div class="post">
+            <div class="text">Terimakasih atas masukannya!</div>
+            <div class="edit">Edit</div>
         </div>
-      </div>
+
+        <div class="star-widget">
+            <input type="radio" name="rate" id="rate-5">
+            <label for="rate-5" class="fas fa-star"></label>
+            <input type="radio" name="rate" id="rate-4">
+            <label for="rate-4" class="fas fa-star"></label>
+            <input type="radio" name="rate" id="rate-3">
+            <label for="rate-3" class="fas fa-star"></label>
+            <input type="radio" name="rate" id="rate-2">
+            <label for="rate-2" class="fas fa-star"></label>
+            <input type="radio" name="rate" id="rate-1">
+            <label for="rate-1" class="fas fa-star"></label>
+            <form action="#">
+                <header></header>
+                <div class="textarea">
+                    <textarea name="textarea" id="" cols="30" placeholder="Beri Kami Masukan"></textarea>
+                </div>
+                <div class="btn">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script>
+        const btn = document.querySelector("button");
+        const post = document.querySelector(".post");
+        const widget = document.querySelector(".star-widget");
+        const editBtn = document.querySelector(".edit");
+        btn.onclick = ()=>{
+            widget.style.display = "none";
+            post.style.display = "block";
+        editBtn.onclick = ()=>{
+            widget.style.display = "block";
+            post.style.display = "none  ";
+            }
+        return false;
+        }
+    </script>
 </body>
 
 </html>
